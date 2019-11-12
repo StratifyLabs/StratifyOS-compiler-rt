15f3bf86ba77bf14d65e747bd811d934495ab664
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2019-11-11 21:54:38 -0700

    CMAKE API updates

--- arm.cmake
+++ arm.cmake
@@ -3,11 +3,11 @@
 #Add sources to the project
 set(SOURCES_PREFIX ${CMAKE_SOURCE_DIR}/src)
 add_subdirectory(src)
-set(SOS_LIB_SOURCELIST ${SOURCES})
+set(SOS_SOURCELIST ${SOURCES})
 
 
 # Lib C
-set(SOS_LIB_TYPE release)
-set(SOS_LIB_OPTION kernel)
-set(SOS_LIB_INCLUDE_DIRECTORIES include)
+set(SOS_CONFIG release)
+set(SOS_OPTION kernel)
+set(SOS_INCLUDE_DIRECTORIES include)
 include(${SOS_TOOLCHAIN_CMAKE_PATH}/sos-lib-std.cmake)
