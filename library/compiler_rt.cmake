31cd3eecfbafe735816c4ce35a55bea62d248133
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2022-08-03 20:10:50 -0500

    port to CMakeSDK v2.0

--- library/compiler_rt.cmake
+++ library/compiler_rt.cmake
@@ -1,5 +1,5 @@
 
 if(NOT DEFINED IS_SDK)
 	set(COMPILER_RT_CONFIG_LIST release debug)
-	sos_sdk_include_target(compiler_rt "${COMPILER_RT_CONFIG_LIST}")
+	cmsdk_include_target(compiler_rt "${COMPILER_RT_CONFIG_LIST}")
 endif()
